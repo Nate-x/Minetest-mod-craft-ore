e7b884f4fe38e5ab604bb3bc760b7e93d3595fbe
Nate-x <37444555+Nate-x@users.noreply.github.com>
2018-03-16 15:15:58 +0200

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,83 @@
+minetest.register_craft({
+	output = "default:stone_with_coal 1",
+	type = "shapeless",
+	recipe = {"default:coal_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "default:stone_with_iron 1",
+	type = "shapeless",
+	recipe = {"default:iron_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "default:stone_with_tin 1",
+	type = "shapeless",
+	recipe = {"default:tin_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "default:stone_with_copper 1",
+	type = "shapeless",
+	recipe = {"default:copper_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "default:stone_with_gold 1",
+	type = "shapeless",
+	recipe = {"default:gold_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "default:stone_with_mese 1",
+	type = "shapeless",
+	recipe = {"default:mese_crystal", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "default:stone_with_diamond 1",
+	type = "shapeless",
+	recipe = {"default:diamond", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "technic:mineral_lead 1",
+	type = "shapeless",
+	recipe = {"technic:lead_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "technic:mineral_sulfur 1",
+	type = "shapeless",
+	recipe = {"technic:sulfur_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "technic:mineral_zinc 1",
+	type = "shapeless",
+	recipe = {"technic:zinc_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "technic:mineral_chromium 1",
+	type = "shapeless",
+	recipe = {"technic:chromium_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "technic:mineral_uranium 1",
+	type = "shapeless",
+	recipe = {"technic:uranium_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "moreores:mineral_silver 1",
+	type = "shapeless",
+	recipe = {"moreores:silver_lump", "default:stone"}
+})
+
+minetest.register_craft({
+	output = "moreores:mineral_mithril 1",
+	type = "shapeless",
+	recipe = {"moreores:mithril_lump", "default:stone"}
+})
